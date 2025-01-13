90e4abecb6ea6fa9a2875b61f13a354d9e5e41c1
Melanie <silvamelanie199@gmail.com>
2025-01-13 12:32:26 -0500

    primer commit

--- models/conexion.php
+++ models/conexion.php
@@ -1,19 +1,21 @@
 <?php
-class conexion{
+class Conexion{
     public function conectar(){
-        $servername="bhlut4j4kuhxsivd4hrp-mysql.services.clever-cloud.com";
-        $username="uvhyhfvxsavrkktw";
-        $password="2w5zMYBaRxgZ5aeDVXTw";
-        $dbname="bhlut4j4kuhxsivd4hrp";
-        $conn=mysqli_connect($servername,$username,$password,$dbname);
-        if(!$conn){
-            echo("Error en la conexion".mysqli_connect_error());
-        }else{
-            return $conn;
+        $servername = "bhlut4j4kuhxsivd4hrp-mysql.services.clever-cloud.com";
+        $username = "uvhyhfvxsavrkktw";
+        $password = "2w5zMYBaRxgZ5aeDVXTw";
+        $dbname = "bhlut4j4kuhxsivd4hrp";
+        
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        
+        if (!$conn) {
+            // Registrar el error o lanzar una excepción
+            die("Error en la conexión: " . mysqli_connect_error());
         }
-     
+        
+        return $conn;
     }
-
 }
+
 
 ?>
